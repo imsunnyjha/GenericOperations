848a5fa477931193ec90d9274033f50391db94a8
SunnyJha <sunnyjha11897@gmail.com>
2020-10-20 10:17:53 +0530

    [SUNNY]Add. UseCase2 for finding maximum double among 3 numbers

--- OperationsWithGenerics/Program.cs
+++ OperationsWithGenerics/Program.cs
@@ -7,7 +7,8 @@ namespace OperationsWithGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generic operations!");
-            MaximumInteger();    
+            MaximumInteger();
+            MaximumDouble();
         }
         public static void MaximumInteger()
         {
@@ -23,6 +24,30 @@ namespace OperationsWithGenerics
             secondNumber = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter third number : ");
             thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            //finding largest number 
+            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+                largestNumber = firstNumber;
+            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+                largestNumber = secondNumber;
+            else largestNumber = thirdNumber;
+
+            Console.WriteLine(largestNumber);
+        }
+        public static void MaximumDouble()
+        {
+            double firstNumber;
+            double secondNumber;
+            double thirdNumber;
+            double largestNumber;
+
+            //input the numbers
+            Console.Write("Enter first number : ");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter third number : ");
+            thirdNumber = Convert.ToDouble(Console.ReadLine());
 
             //finding largest number 
             if (firstNumber > secondNumber && firstNumber > thirdNumber)
