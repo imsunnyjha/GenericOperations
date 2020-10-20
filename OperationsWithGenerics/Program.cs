d66b9143c7b82628437fd46a619b34bb26a78cca
SunnyJha <sunnyjha11897@gmail.com>
2020-10-20 10:13:23 +0530

    [SUNNY]Add. UseCase1 for finding maximum integer among 3 numbers

--- OperationsWithGenerics/Program.cs
+++ OperationsWithGenerics/Program.cs
@@ -7,6 +7,31 @@ namespace OperationsWithGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generic operations!");
+            MaximumInteger();    
+        }
+        public static void MaximumInteger()
+        {
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+            int largestNumber;
+
+            //input the numbers
+            Console.Write("Enter first number : ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number : ");
+            thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            //finding largest number 
+            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+                largestNumber = firstNumber;
+            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+                largestNumber = secondNumber;
+            else largestNumber = thirdNumber;
+
+            Console.WriteLine(largestNumber);
         }
     }
 }
