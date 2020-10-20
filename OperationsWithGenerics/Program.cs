b20398c284fd1101c9e434717536d3472b9053a2
SunnyJha <sunnyjha11897@gmail.com>
2020-10-20 09:53:12 +0530

    [SUNNY]Add. Welcome to generic operations

---
+++ OperationsWithGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperationsWithGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to generic operations!");
+        }
+    }
+}
